799ff0e93ad6847f83198a4889f62fe481b0c4fd
Alexandre Oliveira Queiroz <alezoq86@gmail.com>
2021-10-04 13:39:59 -0400

    Delecao de usuario com DELETE

--- src/main/java/com/alexandreoqueiroz/workshopmongo/services/UserService.java
+++ src/main/java/com/alexandreoqueiroz/workshopmongo/services/UserService.java
@@ -34,5 +34,11 @@ public class UserService {
 			return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 		}
 		
+		public void delete(String id) {
+			findById(id);
+			repo.deleteById(id);
+		}
+		
+		
 		
 }
