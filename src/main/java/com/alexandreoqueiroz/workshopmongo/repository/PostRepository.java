d5f786d4ef4ab1958ecb825c8d157939359b4a89
Alexandre Oliveira Queiroz <alezoq86@gmail.com>
2021-10-05 11:35:48 -0400

    Consulta simples com @Query

--- src/main/java/com/alexandreoqueiroz/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/alexandreoqueiroz/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.alexandreoqueiroz.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.alexandreoqueiroz.workshopmongo.domain.Post;
@@ -10,7 +11,10 @@ import com.alexandreoqueiroz.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
-	List<Post> findByTitleContainingIngoreCase(String text);
+	@Query("{ 'title' : { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
+	//List<Post> findByTitleContainingIngoreCase(String text);
 	
 	
 }
--- src/main/java/com/alexandreoqueiroz/workshopmongo/services/PostService.java
+++ src/main/java/com/alexandreoqueiroz/workshopmongo/services/PostService.java
@@ -22,7 +22,7 @@ public class PostService {
 		}
 		
 		public List<Post> findByTitle(String text){
-			return repo.findByTitleContainingIngoreCase(text);
+			return repo.searchTitle(text);
 		}
 		
 }
